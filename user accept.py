0f72edf1832f92a79a9e700d987b2dcbe7db685f
divya01-art <divyadiv962@gmail.com>
2025-07-20 12:53:57 +0530

    Create user accept.py

---
+++ user accept.py
@@ -0,0 +1,9 @@
+from IPython.display import Image
+try:
+  filename = take_photo()
+  print('Saved to {}'.format(filename))
+
+  # Show the image which was just taken.
+  display(Image(filename))
+except Exception as err:
+    print(str(err))
